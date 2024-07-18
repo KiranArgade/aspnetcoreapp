ca84c5a318b784355017c709cfae07c871fade88
KiranArgade <kiran.argade@nttdata.com>
2024-07-18 11:03:40 +0530

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ namespace aspnetcoreapp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ILogger<IndexModel> _loggerTest;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
