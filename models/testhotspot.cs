413b82f49d1b00d809f6857774dfcdc8328db656
KiranArgade <kiran.argade@nttdata.com>
2025-02-14 11:04:47 +0530

    Create testhotspot.cs

---
+++ models/testhotspot.cs
@@ -0,0 +1,13 @@
+public unsafe int SubarraySum(int[] array, int start, int end)  // Sensitive
+{
+    var sum = 0;
+
+    // Skip array bound checks for extra performance
+    fixed (int* firstNumber = array)
+    {
+        for (int i = start; i < end; i++)
+            sum += *(firstNumber + i);
+    }
+
+    return sum;
+}
